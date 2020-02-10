fbbb902ca49b465b3025faad72d575efd0cdff3f
janchk <janchk@gmail.com>
2020-02-10 14:24:16 +0300

    additions for clean install

--- tensorflow/bin/build.sh
+++ tensorflow/bin/build.sh
@@ -41,7 +41,7 @@ then
 fi
 
 # Download and install system dependencies
-sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip python python3 autoconf libtool curl automake make
+sudo apt-get install -y wget pkg-config zip g++ zlib1g-dev unzip python python3 autoconf libtool curl automake make
 
 # Download and install Bazel
 if ! [ -x "$(command -v bazel)" ];
