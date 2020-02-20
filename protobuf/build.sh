7b329fda5b7d79b1f29834b2e3024b52b56b2aa0
Jan Akhremchik <janchk@gmail.com>
2020-02-20 17:05:48 +0300

    Update protobuf version

--- protobuf/build.sh
+++ protobuf/build.sh
@@ -6,7 +6,7 @@ echo "TensorFlow: Build: Cloning Protobuf";
 git clone https://github.com/protocolbuffers/protobuf.git ${TF_SRC};
 cd protobuf
 git submodule update --init --recursive
-git checkout v3.7.1
+git checkout v3.8.0
 ./autogen.sh
 ./configure
 
